942f1ec7968728516ce754bc2d6b23386066d533
cheorhyeon <r4560798@naver.com>
2025-06-07 16:24:16 +0900

    refactor : 누락된 swagger 예시 및 설정 추가

--- src/main/java/com/example/board/dto/BoardDetail.java
+++ src/main/java/com/example/board/dto/BoardDetail.java
@@ -2,13 +2,18 @@ package com.example.board.dto;
 
 import com.example.board.entity.Board;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record BoardDetail(
+	@Schema(description = "게시글 Id", example = "1")
 	Long id,
+	@Schema(description = "게시글 제목", example = "제목입니다.")
 	String title,
+	@Schema(description = "게시글 내용", example = "내용입니다.")
 	String content,
+	@Schema(description = "게시글 작성자", example = "홍길동")
 	String author
 ) {
 	public static BoardDetail from(Board board) {
